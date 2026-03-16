6bb6c2992b57815af6e520901c6ae2f16835cb34
Greg Bowler <greg@g105b.com>
2026-03-16 19:10:15 +0000

    feature: implement pub:sub model closes #3

---
+++ src/Event/EventDispatcher.php
@@ -0,0 +1,22 @@
+<?php
+namespace Gt\Async\Event;
+
+class EventDispatcher {
+	/** @var array<string, array<int, callable>> */
+	private array $subscriberList;
+
+	public function __construct() {
+		$this->subscriberList = [];
+	}
+
+	public function subscribe(string $eventName, callable $callback):void {
+		$this->subscriberList[$eventName] ??= [];
+		$this->subscriberList[$eventName][] = $callback;
+	}
+
+	public function publish(Event $event):void {
+		foreach($this->subscriberList[$event->getName()] ?? [] as $callback) {
+			call_user_func($callback, $event);
+		}
+	}
+}
