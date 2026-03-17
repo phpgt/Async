669766f1f2a1e22a6c7b5799e9415e163f4c5b18
Greg Bowler <greg@g105b.com>
2026-03-17 09:36:12 +0000

    Event model (#41)
    * build: upgrade dependencies
    
    * test: isolate functionality for #3
    
    * feature: implement pub:sub model
    closes #3
    
    * test: php 8.2

---
+++ src/Event/Event.php
@@ -0,0 +1,20 @@
+<?php
+namespace Gt\Async\Event;
+
+class Event {
+	private string $name;
+	private mixed $detail;
+
+	public function __construct(string $name, mixed $detail = null) {
+		$this->name = $name;
+		$this->detail = $detail;
+	}
+
+	public function getName():string {
+		return $this->name;
+	}
+
+	public function getDetail():mixed {
+		return $this->detail;
+	}
+}
